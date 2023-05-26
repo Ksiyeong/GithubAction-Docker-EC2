5f1d06b0ad3791c1228c1543a600a0f47103a7bf
Ksiyeong <yuiop2767@naver.com>
2023-05-26 17:15:36 +0900

    [chore] 초기세팅

---
+++ src/main/java/com/example/demo/HelloController.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/")
+    public ResponseEntity hello() {
+        return ResponseEntity.ok("hello world");
+    }
+}
