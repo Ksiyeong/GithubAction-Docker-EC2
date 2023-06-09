01b05397ff92d0481ecf0ffbe072cbfed3484d60
Ksiyeong <yuiop2767@naver.com>
2023-06-09 13:19:08 +0900

    [test] 배포 자동화 테스트2

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -8,6 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping("/")
     public ResponseEntity hello() {
-        return ResponseEntity.ok("자동 배포 테스트");
+        return ResponseEntity.ok("자동 배포 테스트2");
     }
 }
